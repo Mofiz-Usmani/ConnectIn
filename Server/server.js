874cf58df9fab52a3bc0e5bc3defa2869bd24c9a
Jackal <mofizusmani1@gmail.com>
2025-11-05 12:00:48 +0530

    register form

--- Server/server.js
+++ Server/server.js
@@ -5,11 +5,27 @@ const path = require("path");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "../Client")));
 
 
 app.get("/", (req, res) => {
     res.render("home");
 });
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Client/index.html"));
+});
+
+
+app.post("/resgister", (req, res) => {
+    let {user, password} = (req.body);
+    console.log(`User : ${user}`);
+    console.log(`password : ${password}`);
+    res.send(`Welcome ${user}`);
+});
+
+
+
 
 app.use((req, res) => {
     res.status(404).send("404 Not Found");
