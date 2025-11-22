10d24c97dc9faa713ac38d4536285a253c82a667
Jackal <mofizusmani1@gmail.com>
2025-11-22 11:57:23 +0530

    Initial commit for ConnectIn project

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+
+
+
+const port = 5000;
+
+
+
+app.get("/", (req, res) => {
+    res.send("Hello, I am Root Server");
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
